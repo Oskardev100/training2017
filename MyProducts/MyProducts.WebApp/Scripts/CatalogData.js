a8ccc6661d34e0f2026533117d255c72398a1cae
Charles Paramo, Oskar <oskar.charles@gmail.com>
2017-08-09 20:08:36 -0500

    Adding Details Page

---
+++ MyProducts/MyProducts.WebApp/Scripts/CatalogData.js
@@ -0,0 +1,24 @@
+﻿//Your jSON Object
+var MyCatalogData = {
+  "Product1":
+  {
+    "id": 0,
+    "title": "myTitle0",
+    "description": "myDescription",
+    "imgUrl": "Images/img_001.jpg"
+  },
+  "Product2":
+  {
+    "id": 1,
+    "title": "myTitle1",
+    "description": "myDescription",
+    "imgUrl": "Images/img_002.jpg"
+  },
+  "Product3":
+  {
+    "id": 2,
+    "title": "myTitle2",
+    "description": "myDescription",
+    "imgUrl": "Images/img_003.jpg"
+  }
+}
